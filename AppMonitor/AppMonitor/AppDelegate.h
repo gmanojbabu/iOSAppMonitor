d383abfa6d0432e36e908a884850e8ff23842be5
Manoj Babu <mbabu@sapient.com>
2014-12-11 17:46:10 +0530

    Created App Monitor iOS single view project

---
+++ AppMonitor/AppMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppMonitor
+//
+//  Created by Manoj Goulla Babu on 12/11/14.
+//  Copyright (c) 2014 Manoj Goulla Babu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppMonitor/AppMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppMonitor
+//
+//  Created by Manoj Goulla Babu on 12/11/14.
+//  Copyright (c) 2014 Manoj Goulla Babu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
